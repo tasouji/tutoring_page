85ea844f7190c0157f55316accceca61289ae92a
Emirhan Tasouji <emirhan.tasouji@mail.utoronto.ca>
2025-05-20 19:33:49 -0400

    Contact page added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tutoring_page
 Official Website of Marand Tutoring - by Tasouji
+
+Using "React Scheduler" by @aldabil
+
---
+++ contact.js
@@ -0,0 +1,14 @@
+import { Scheduler } from "@aldabil/react-scheduler";
+
+<Scheduler
+    view="month"
+    events={[
+        {
+            event_id: 1,
+            title: "Event 1",
+            start: new Date("2025/5/15 09:00"),
+            end: new Date("2025/8/15 18:00"),
+        }
+    ]}
+    />
+    
