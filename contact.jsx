a2ee85e2ba2b7a9992532add3132eb1556f4c9ce
Emirhan Tasouji <emirhan.tasouji@mail.utoronto.ca>
2025-05-21 15:34:55 -0400

    Updated the JSX file

--- contact.jsx
+++ contact.jsx
@@ -1,17 +1,16 @@
-import React from "https://esm.sh/react";
-import { createRoot } from "https://esm.sh/react-dom/client";
-import { Scheduler } from "https://esm.sh/@aldabil/react-scheduler";
-
-const root = createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const { Scheduler } = window["@aldabil/react-scheduler"];
 
 root.render(
-  React.createElement(Scheduler, {
-    view: "month",
-    events: [{
-      event_id: 1,
-      title: "Event 1",
-      start: new Date("2025-05-15T09:00:00"),
-      end:   new Date("2025-08-15T18:00:00"),
-    }],
-  })
-);
+  <Scheduler
+    view="month"
+    events={[
+      {
+        event_id: 1,
+        title: "Lectures",
+        start: new Date("2025-05-15T09:00:00"),
+        end: new Date("2025-06-15T18:00:00"),
+      },
+    ]}
+    />
+  );
