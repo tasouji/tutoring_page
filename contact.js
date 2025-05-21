05bbe6138b3ee9e52833b7b07a0329cbdc2ab356
Emirhan Tasouji <emirhan.tasouji@mail.utoronto.ca>
2025-05-21 11:31:05 -0400

    Removed Importmaps for Safari users

--- contact.js
+++ contact.js
@@ -1,20 +1,17 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Scheduler } from "@aldabil/react-scheduler";
+import React from "https://esm.sh/react";
+import { createRoot } from "https://esm.sh/react-dom/client";
+import { Scheduler } from "https://esm.sh/@aldabil/react-scheduler";
 
-const rootEl = document.getElementById("root");
-const root = createRoot(rootEl);
+const root = createRoot(document.getElementById("root"));
 
 root.render(
-    React.createElement(Scheduler, {
+  React.createElement(Scheduler, {
     view: "month",
-    events: [
-        {
-            event_id: 1,
-            title: "Event 1",
-            start: new Date("2025-05-15T09:00:00"),
-            end: new Date("2025-08-15T18:00:00"),
-        }
-    ],
-})
+    events: [{
+      event_id: 1,
+      title: "Event 1",
+      start: new Date("2025-05-15T09:00:00"),
+      end:   new Date("2025-08-15T18:00:00"),
+    }],
+  })
 );
