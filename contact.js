1f6b45ceddadc1588008e1835673a86b5589b43f
Emirhan Tasouji <emirhan.tasouji@mail.utoronto.ca>
2025-05-20 20:10:16 -0400

    Added ImportMaps

--- contact.js
+++ contact.js
@@ -1,14 +1,20 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
 import { Scheduler } from "@aldabil/react-scheduler";
 
-<Scheduler
-    view="month"
-    events={[
+const rootEl = document.getElementById("root");
+const root = createRoot(rootEl);
+
+root.render(
+    React.createElement(Scheduler, {
+    view: "month",
+    events: [
         {
             event_id: 1,
             title: "Event 1",
-            start: new Date("2025/5/15 09:00"),
-            end: new Date("2025/8/15 18:00"),
+            start: new Date("2025-05-15T09:00:00"),
+            end: new Date("2025-08-15T18:00:00"),
         }
-    ]}
-    />
-    
+    ],
+})
+);
